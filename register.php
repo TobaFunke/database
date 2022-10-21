0fc93e3c42b4ebffaa1d914a21b1d963e824782d
TobaFunke <104855662+TobaFunke@users.noreply.github.com>
2022-10-21 18:26:52 +0100

    Update register.php

--- register.php
+++ register.php
@@ -4,15 +4,34 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    $host = "localhost";
+          $username = "forms";
+          $password = "password";
+          $db = "html_form";
+          $conn = mysqli_connect($host, $username, $password, $db);
+
 registerUser($username, $email, $password);
 
 }
 
 function registerUser($username, $email, $password){
     //save data into the file
-    
+    if (!$con)
+          {
+die("Connection failed!" . mysqli_connect_error());
+          }
+          $query = "INSERT INTO simple_form (id, username, email, comment VALUES $username, $email, $comment";
+          $run = mysqli_query($conn, $query);
+if (run)
     // echo "OKAY";
+}{
+    echo "User Successfully Registered";
 }
-echo "HANDLE THIS PAGE";
+
+mysqli_close($conn);
+
+?>
+
+
 
 
